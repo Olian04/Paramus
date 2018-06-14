b127803628eabdb1b254b8f64a86c1d65e3ce4e8
Oliver <rift95@live.se>
2018-06-14 23:58:27 +0200

    should now bind it self to the global window object

--- src/paramus.ts
+++ src/paramus.ts
@@ -2,6 +2,15 @@ import { Internal, IParamus } from './index';
 export { IStore } from './index';
 
 export const Paramus: IParamus = Internal();
+export default Paramus;
+declare global {
+    interface Window {
+        Paramus: IParamus;
+    }
+}
+if (typeof window !== 'undefined') {
+    window.Paramus = Paramus;
+}
 
 import { ObjectStore } from './stores/object';
 Paramus.extend( new ObjectStore() );
