1c24d2703305eb6ebaabc9e466d89938000410c9
Oliver Anteros <rift95@live.se>
2018-06-15 00:10:29 +0200

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -1,21 +1,23 @@
-const increment = document.createElement('button');
-const decrement = document.createElement('button');
-const output = document.createElement('p');
-const root = document.getElementById('root');
+window.onload = function() {
+	const increment = document.createElement('button');
+	const decrement = document.createElement('button');
+	const output = document.createElement('p');
+	const root = document.getElementById('root');
 
-const store = Paramus('url', { 
-	count: 0
-}, store => {
-  output.innerHTML = '' + store.count;
-});
+	const store = Paramus('url', { 
+		count: 0
+	}, store => {
+	  output.innerHTML = '' + store.count;
+	});
 
-root.innerHTML = '';
-root.appendChild(increment);
-root.appendChild(decrement);
-root.appendChild(output);
-	
-increment.innerHTML = '+';
-increment.onclick = () => store.count++;
-	
-decrement.innerHTML = '-';
-decrement.onclick = () => store.count--;
+	root.innerHTML = '';
+	root.appendChild(increment);
+	root.appendChild(decrement);
+	root.appendChild(output);
+
+	increment.innerHTML = '+';
+	increment.onclick = () => store.count++;
+
+	decrement.innerHTML = '-';
+	decrement.onclick = () => store.count--;
+}
