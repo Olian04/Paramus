7e7e1121fe47238b8302c2edc85c365d605c578b
Oliver Anteros <rift95@live.se>
2018-06-15 02:01:33 +0200

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -1,15 +1,15 @@
 window.onload = function() {
 	const increment = document.createElement('button');
 	const decrement = document.createElement('button');
-	const output = document.createElement('p');
+	const output = document.createElement('span');
 	const root = document.getElementById('root');
 
 	increment.innerHTML = '+';
 	decrement.innerHTML = '-';
 	root.innerHTML = '';
-	root.appendChild(increment);
 	root.appendChild(decrement);
 	root.appendChild(output);
+	root.appendChild(increment);
 
 	Paramus('url', { 
 		count: 0
